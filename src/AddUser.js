747de2a7d0c58e7ce89bedadc2f1b1e6a064f2a2
Jenny Chou <jmychou@gmail.com>
2020-06-10 20:10:44 -0700

    Add Event Handler to AddUser.js

--- src/AddUser.js
+++ src/AddUser.js
@@ -8,13 +8,13 @@ class AddUser extends Component {
         this.state = {
              userName: '',          // To allow for modifying state, these property names are assigned as values to rendered input elements.   
              id: '',                // Then, we add an event handler fxn to handle the event of entering text into input elements
-        }
+        };
     }
     
     // Event handler: When called, this function modifies the state's username to equal the event target's value. To call, need to add an event listener (ex. in input element)
-    handleUserNameChange(e) {           // e is for event; like any param, it's arbitrary, but it lets us know what we have access to (ex. input's name, value, etc)
-        this.setState({                 // setState includes {} because we're creating a new object 
-            userName: e.target.value,
+    handleChange(e) {           // e is for event; like any param, it's arbitrary, but it lets us know what we have access to (ex. input's name, value, etc)
+        this.setState({                       // setState includes {} because we're creating a new object 
+            [e.target.name]: e.target.value,  // using [e.target.name] rather than username or id allows you to reuse this method for multiple elements; also need to have a name attribute in each input element whose value is a state property name
         });               
     }
     
@@ -27,8 +27,9 @@ class AddUser extends Component {
                         type="text" 
                         className="form-control" 
                         aria-describedby="username" 
-                        value={this.state.userName}             {/* value attribute must match a state property name */}
-                        onChange={this.handleUserNameChange}
+                        name="userName"
+                        value={this.state.userName}                 // value attribute must match a state property name
+                        onChange={this.handleChange.bind(this)}     // Bind this method to THIS component, so when called it knows which state to refer to. Another way to bind is in the constructor by saying this.handleChange = this.handlChange.bind(this)
                     />
                 </div>
                 <div className="form-group">
@@ -37,8 +38,9 @@ class AddUser extends Component {
                         type="text" 
                         className="form-control" 
                         area-describedby="id" 
-                        value={this.state.id}               {/* value attribute must match a state property name */}
-                        
+                        name="id"
+                        value={this.state.id}               // value attribute must match a state property name
+                        onChange={this.handleChange.bind(this)}
                     />
                 </div>
                 
